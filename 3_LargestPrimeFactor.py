66b48be337785553d331b6ba2402d3a0a158c642
Marco Rossi <marcoreds9724@gmail.com>
2021-07-30 21:54:33 +0200

    Problem 3: Largest prime factory of a number

---
+++ 3_LargestPrimeFactor.py
@@ -0,0 +1,19 @@
+# The prime factors of 13195 are 5, 7, 13 and 29.
+# What is the largest prime factor of the number 600851475143 ?
+
+n = 600851475143
+div = 2
+maxFact = 0
+while (n != 0):
+    if (n % div != 0):
+        div += 1
+    else:
+        maxFact = n
+        n /= div
+        if (n == 1):
+            print("The largest prime factor is: ", maxFact)
+            break
+
+
+
+
--- README.md
+++ README.md
@@ -4,3 +4,5 @@ Repository to contain development of projecteuler website problems
 #### 1 - Sum of multiple of 3 or 5 - 26/07/21
 
 #### 2 - Even Fibonacci numbers - 27/07/21
+
+#### 3 - Largest Prime Factory of a number - 30/07/21
